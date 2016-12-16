41b45dd1db82b1cd9171c397b45f870bd026c307
pangfuli <pflnh@me.com>
2016-12-16 22:47:12 +0800

    提交

--- app.js
+++ app.js
@@ -12,16 +12,12 @@ var router = require('./router')
 var app = new Koa()
 app.use(router.routes())
 
-// app.use(wechat(config.wechat, weixin.reply))
-// var sendfile = require('koa-sendfile')
+app.use(wechat(config.wechat, weixin.reply))
 
-// app.use(function* (next) {
-//   var stats = yield sendfile(this, "/Users/fulipang/Projects/Wechat/uploads/default.jpg")
-//   if (!this.status) this.throw(404)
-// })
+
+
+
 app.listen(3030)
-
-
 console.log("Listen: 3030")
 
 
