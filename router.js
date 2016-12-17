cbb566dc71bee8f174715ce5ef9e34df9b784ac1
pangfuli <pflnh@me.com>
2016-12-17 20:53:36 +0800

    tij

--- router.js
+++ router.js
@@ -12,10 +12,10 @@ var formidable = require('formidable');
 // 第一个参数为别名, 第二个为路径匹配
 Router.get('file', '/files/:id',function * (next) {
     var despath = path.join(__dirname,('./uploads/' + this.params.id)); 
-      var filepath = yield getFilePath(despath)
-      yield sendfile(this, filepath);
-      yield next;
-      console.log("filepath: " + filepath) 
+  var filepath = yield getFilePath(despath)
+  yield sendfile(this, filepath);
+  yield next;
+  console.log("filepath: " + filepath) 
 
 });
 
@@ -34,20 +34,61 @@ function *getFilePath(despath) {
 
 // 存储微信图文信息
 Router.post('/api/material/:id',function *(next) {
-	var form = new formidable.IncomingForm();
-	form.parse(this.req, function (error, fields, files) {
-		console.log('fields: ', JSON.stringify(fields));
+	var file = yield parseFile(this.req)
+	var news = yield saveNews(file.fields);
+	yield saveImage(file.files.files, news);
+	this.body = 'success\n';
+})
+
+function *parseFile (req) {
+	return new Promise(function (resolve, reject) {
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (error, fields, files) {
+			console.log('fields: ', JSON.stringify(fields));
+			console.log('files: ', JSON.stringify(files));
+			if (error) {return reject(error)}
+			resolve({files:files, fields: fields})
+		})
+	}).then(function (file) {
+		return file	
+	})
+}
+
+function * saveNews(fields) {
+	return new Promise(function (resolve, reject) {
 		new News({
 			title: fields.title || "",
 			description: fields.description || "",
 			picUrl: fields.picUrl || "http://sanwenzx.com/uploads/allimg/100828/09442S544-0.jpg",
 			url: fields.url ||  "https://wwww.baidu.com",
 		}).save(function (error, news) {
-			console.log('news title:', news.title);
+			if (error) {return reject(error)}
+			resolve(news);
+		}).then(function (news) {
+			return news
 		})
 	})
-	this.body = 'success';
-})
+}
+
+function *saveImage(files, news) {
+	var from = files.path;
+	var type = files.type || 'image/jpg';
+	var ext = type.split('/')[1] || 'jpg'
+  	ext = '.' + ext;
+	return new Promise(function (resolve, reject) {
+	  	if (ext.length > 4) {ext = '.jpg'};
+		var to = __dirname + '/uploads/' + news._id + ext
+		fs.rename(from, to, function (error) {
+			if (error) {return reject(error)}
+			resolve(to);
+		})
+	}).then(function (filepath) {
+		news.picUrl = 'https://45.124.66.158:3030/files/' + news._id + ext;
+					console.log('news: ', JSON.stringify(news));
+		return filepath
+	})
+	
+}
 
 
 Router.get('/',function *(next) {
@@ -58,3 +99,8 @@ Router.get('/',function *(next) {
 })
 
 exports = module.exports = Router;
+
+
+
+
+
